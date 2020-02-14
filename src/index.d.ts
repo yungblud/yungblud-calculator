06782bf9cb79bd39747fa1c0d83f176bd090e258
Dongho Choi <kkiathorn@gmail.com>
2020-02-14 11:59:34 +0900

    apply index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,6 @@
 declare module 'yungblud-calculator' {
-    const add = (a: number, b: number) => number;
-    const sub = (a: number, b: number) => number;
-    const mul = (a: number, b: number) => number;
-    const div = (a: number, b: number) => number;
+    const add: (a: number, b: number) => number;
+    const sub: (a: number, b: number) => number;
+    const mul: (a: number, b: number) => number;
+    const div: (a: number, b: number) => number;
 }
